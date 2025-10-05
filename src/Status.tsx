5f519010aa116a63057cad65705690e0d071f191
Antti <hellsten.antti@gmail.com>
2025-10-05 12:51:38 +0300

    fixed message showing before game has started and refactored message logic

--- src/Status.tsx
+++ src/Status.tsx
@@ -1,6 +1,4 @@
-import Button from '@mui/material/Button';
-
-import { GameBoard, Cell, StatusProps, PlayerMark } from './types';
+import { Cell, StatusProps, PlayerMark } from './types';
 import { isTieGame, togglePlayer } from './utils';
 
 const Status = ({ winningValue, nextPlayer, players, grid, gameStarted, moveHistory }: StatusProps) => {
@@ -8,29 +6,39 @@ const Status = ({ winningValue, nextPlayer, players, grid, gameStarted, moveHist
     return togglePlayer(nextPlayer) === PlayerMark.X ? players?.playerOne : players?.playerTwo;
   };
 
-  const getWinningPlayerName = (winningValue: Cell | undefined): string | undefined => {
-    return winningValue === PlayerMark.X
+  const getWinningPlayerName = (winner: Cell): string | undefined => {
+    return winner === PlayerMark.X
       ? players?.playerOne
       : players?.playerTwo
   }
 
-  const getMessage = (winningValue: Cell | undefined, grid: GameBoard) => {
+  const generateStatusMessage = () => {
+    const isGameActiveOrEnded = gameStarted || moveHistory.length > 1;
+  
+    if (!isGameActiveOrEnded) return null;
+    
     if (isTieGame (winningValue, grid)) {
       return <span>Tie game.</span>
     }
     
     if (winningValue) {
-      return <span>Winner is <strong>{getWinningPlayerName(winningValue)}</strong> on turn {moveHistory.length - 1}.</span>
+      return (
+        <span>
+          Winner is <strong>{getWinningPlayerName(winningValue)}</strong> on turn {moveHistory.length - 1}.
+        </span>
+      )
     }
 
-    return <span>Turn <strong>{moveHistory.length}</strong>: You're up <strong>{getNextPlayerName()}</strong>.</span>
+    return (
+      <span>
+        Turn <strong>{moveHistory.length}</strong>: You're up <strong>{getNextPlayerName()}</strong>.
+      </span>
+    )
   };
 
-  return (
-    <p className="status">
-      {gameStarted && getMessage(winningValue, grid)}
-    </p>
-  )
-}
+  const message = generateStatusMessage();
+
+  return message ? <p className="status">{message}</p> : null;
+};
 
 export default Status;
